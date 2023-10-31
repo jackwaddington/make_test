1ad21879fbd5c59d143bfee5fd747c2c47660ab3
Jack Waddington <jack@waddington.me>
2023-10-31 16:32:01 +0200

    Now this is cleaned up

---
+++ ft_main.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_test.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jwadding <jwadding@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/31 11:48:43 by jwadding          #+#    #+#             */
+/*   Updated: 2023/10/31 16:28:00 by jwadding         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_add_one(int* value);
+void	ft_add_two(int* value);
+void	ft_putchar(int c);
+
+int	main(void)
+{
+	int i;
+
+	i = 65;
+
+	ft_putchar(i);
+
+	ft_add_one(&i);
+
+	ft_putchar(i);
+
+	ft_add_two(&i);
+
+	ft_putchar(i);
+}
+
+
